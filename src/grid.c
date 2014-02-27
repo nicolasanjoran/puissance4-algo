196dc8b95d85882fb2209712bc0cb82185549533
nicolasanjoran@gmail.com <nicolasanjoran@gmail.com@edd8afde-9bbc-a688-392e-a57b860c5a2d>
2014-02-27 16:42:06 +0000

    Files & Directories Tree modified. First Code commit.

---
+++ src/grid.c
@@ -0,0 +1,139 @@
+#include "../h/grid.h"
+
+int GRID_getColumnCasesLeft(char grid[][6], int colNum)
+{
+	int result = 0;
+	int i;
+
+	for(i=0 ; i<6 ; i++)
+	{
+		if(grid[colNum][i] == 0)
+		{
+			result++;
+		}
+	}
+	return result;
+}
+
+int GRID_setNextTurn(char grid[][6], int colNum, char currentPlayer)
+{
+	int i;
+
+	for(i=0 ; i<6 ; i++)
+	{
+		if(grid[colNum][i] == 0)
+		{
+			grid[colNum][i] = currentPlayer;
+			return 0;
+		}
+	}
+	return 1;
+}
+
+char GRID_isFinished(char grid[][6])
+{
+	int col=0;
+	int raw=0;
+	int i,j;
+	int currentPlayerChecking=0;
+	int nbCoins=0;
+
+	// Vertical checking
+	for(i=0 ; i<7 && nbCoins<4 ; i++)
+	{
+		for(j=0 ; j<6 && nbCoins<4 ; j++)
+		{	
+			if(grid[i][j]==currentPlayerChecking)
+			{
+				nbCoins++;
+			}else{
+				currentPlayerChecking=grid[i][j];
+				nbCoins=0;
+			}
+		}
+	}
+
+	// Horizontal checking
+	for(i=0 ; i<6 && nbCoins<4 ; i++)
+	{
+		for(j=0 ; j<7 && nbCoins<4 ; j++)
+		{	
+			if(grid[j][i]==currentPlayerChecking)
+			{
+				nbCoins++;
+			}else{
+				currentPlayerChecking=grid[j][i];
+				nbCoins=0;
+			}
+		}
+	}
+
+
+	// Diagonal checking n°1
+	i=2;
+	j=0;
+	while(j<3 && nbCoins<4)
+	{
+		col=j;
+		raw=i;
+
+		while(col<7 && raw <6 && nbCoins<4)
+		{
+			if(grid[col][raw]==currentPlayerChecking)
+			{
+				nbCoins++;
+			}else{
+				currentPlayerChecking=grid[col][raw];
+				nbCoins=0;
+			}
+
+			col++;
+			raw++;
+		}
+
+		if(i>0)
+		{
+			i--;
+		}else{
+			j++;
+		}
+	}
+
+	// Diagonal checking n°2
+	i=3;
+	j=0;
+	while(j<3 && nbCoins<4)
+	{
+		col=j;
+		raw=i;
+
+		while(col<7 && raw >=0 && nbCoins<4)
+		{
+			if(grid[col][raw]==currentPlayerChecking)
+			{
+				nbCoins++;
+			}else{
+				currentPlayerChecking=grid[col][raw];
+				nbCoins=0;
+			}
+
+			col++;
+			raw--;
+		}
+
+		if(i<6)
+		{
+			i++;
+		}else{
+			j++;
+		}
+	}
+
+	// Check if 4-coins raw is found
+	if(nbCoins==4)
+	{
+		return 1;
+	}else{
+		return 0;
+	}
+}
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../h/game.h"
+
+int main (int argc, char *argv[])
+{
+	printf("---------- PUISSANCE 4 ---------\n");
+	GAME_main('a','a');
+	return 0;
+}
