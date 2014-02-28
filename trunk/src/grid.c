27d423fab4f995b9f1f5cc906473c811963c2ba7
nicolasanjoran@gmail.com <nicolasanjoran@gmail.com@edd8afde-9bbc-a688-392e-a57b860c5a2d>
2014-02-28 15:36:59 +0000

    Code improved with minimax implement. Grid isFinished() to be improved... Some errors happen when 4 coins follow 1 opposite color coin.

--- trunk/src/grid.c
+++ trunk/src/grid.c
@@ -30,6 +30,117 @@ int GRID_setNextTurn(char grid[][6], int colNum, char currentPlayer)
 	return 1;
 }
 
+int GRID_getMaxLengthRaw(char grid[][6], char player)
+{
+	int col=0;
+	int raw=0;
+	int i,j;
+	int currentPlayerChecking=0;
+	int nbCoins=0;
+	int gameFinished = 0;
+	int maxLength = 0;
+
+	// Vertical checking
+	for(i=0 ; i<7 && nbCoins<4 ; i++)
+	{
+		currentPlayerChecking = grid[i][0];
+		for(j=0 ; j<6 && nbCoins<4 ; j++)
+		{	
+			if(currentPlayerChecking != 0 && grid[i][j]==player)
+			{
+				nbCoins++;
+			}else{
+				currentPlayerChecking=grid[i][j];
+				nbCoins=0;
+			}
+		}
+		if(nbCoins > maxLength) maxLength = nbCoins;
+	}
+	
+	// Horizontal checking
+	for(i=0 ; i<6 && nbCoins<4 ; i++)
+	{
+		nbCoins=0;
+		currentPlayerChecking = grid[i][0];
+		for(j=0 ; j<7 && nbCoins<4 ; j++)
+		{	
+			if(currentPlayerChecking != 0 && grid[j][i]==player)
+			{
+				nbCoins++;
+			}else{
+				currentPlayerChecking=grid[j][i];
+				nbCoins=0;
+			}
+		}
+		if(nbCoins > maxLength) maxLength = nbCoins;
+	}
+
+	// Diagonal checking n°1
+	i=2;
+	j=0;
+	while(j<3 && nbCoins<4)
+	{
+		nbCoins=0;
+		col=j;
+		raw=i;
+		currentPlayerChecking = grid[col][raw];
+		while(col<7 && raw <6 && nbCoins<4)
+		{
+			if(currentPlayerChecking != 0 && grid[col][raw]==player)
+			{
+				nbCoins++;
+			}else{
+				currentPlayerChecking=grid[col][raw];
+				nbCoins=0;
+			}
+
+			col++;
+			raw++;
+		}
+
+		if(i>0)
+		{
+			i--;
+		}else{
+			j++;
+		}
+		if(nbCoins > maxLength) maxLength = nbCoins;
+	}
+
+	// Diagonal checking n°2
+	i=3;
+	j=0;
+	while(j<3 && nbCoins<4)
+	{
+		nbCoins=0;
+		col=j;
+		raw=i;
+		currentPlayerChecking = grid[col][raw];
+		while(col<7 && raw >=0 && nbCoins<4)
+		{
+			if(currentPlayerChecking != 0 && grid[col][raw]==player)
+			{
+				nbCoins++;
+			}else{
+				currentPlayerChecking=grid[col][raw];
+				nbCoins=0;
+			}
+
+			col++;
+			raw--;
+		}
+
+		if(i<6)
+		{
+			i++;
+		}else{
+			j++;
+		}
+		if(nbCoins > maxLength) maxLength = nbCoins;
+	}
+	return maxLength;
+}
+
 char GRID_isFinished(char grid[][6])
 {
 	int col=0;
@@ -138,8 +249,8 @@ char GRID_isFinished(char grid[][6])
 	// Check if 4-coins raw is found
 	if(nbCoins==4)
 	{
-		printf("Player %d WON\n", currentPlayerChecking);
-		return 1;
+		//printf("Player %d WON\n", currentPlayerChecking);
+		return currentPlayerChecking;
 	}
 	return 0;
 }
