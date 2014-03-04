ba5ee535a87add6522f4f579e73ab4d458fb3fd3
nicolasanjoran@gmail.com <nicolasanjoran@gmail.com@edd8afde-9bbc-a688-392e-a57b860c5a2d>
2014-03-04 18:54:38 +0000

    GRID_isFinished() updated: 	- Bug Fixed : 5 coins detection for winning tour instead of 4

--- src/grid.c
+++ src/grid.c
@@ -46,13 +46,15 @@ int GRID_getMaxLengthRaw(char grid[][6], char player)
 		currentPlayerChecking = grid[i][0];
 		for(j=0 ; j<6 && nbCoins<4 ; j++)
 		{	
-			if(currentPlayerChecking != 0 && grid[i][j]==player)
+			if(grid[i][j] != currentPlayerChecking)
 			{
-				nbCoins++;
-			}else{
-				currentPlayerChecking=grid[i][j];
 				nbCoins=0;
 			}
+			if(grid[i][j] == player)
+			{
+				nbCoins++;
+			}
+			currentPlayerChecking=grid[i][j];
 		}
 		if(nbCoins > maxLength) maxLength = nbCoins;
 	}
@@ -64,13 +66,15 @@ int GRID_getMaxLengthRaw(char grid[][6], char player)
 		currentPlayerChecking = grid[i][0];
 		for(j=0 ; j<7 && nbCoins<4 ; j++)
 		{	
-			if(currentPlayerChecking != 0 && grid[j][i]==player)
+			if(grid[j][i] != currentPlayerChecking)
 			{
-				nbCoins++;
-			}else{
-				currentPlayerChecking=grid[j][i];
 				nbCoins=0;
 			}
+			if(grid[j][i] == player)
+			{
+				nbCoins++;
+			}
+			currentPlayerChecking=grid[j][i];
 		}
 		if(nbCoins > maxLength) maxLength = nbCoins;
 	}
@@ -86,13 +90,15 @@ int GRID_getMaxLengthRaw(char grid[][6], char player)
 		currentPlayerChecking = grid[col][raw];
 		while(col<7 && raw <6 && nbCoins<4)
 		{
-			if(currentPlayerChecking != 0 && grid[col][raw]==player)
+			if(grid[col][raw] != currentPlayerChecking)
 			{
-				nbCoins++;
-			}else{
-				currentPlayerChecking=grid[col][raw];
 				nbCoins=0;
 			}
+			if(grid[col][raw] == player)
+			{
+				nbCoins++;
+			}
+			currentPlayerChecking=grid[col][raw];
 
 			col++;
 			raw++;
@@ -156,13 +162,15 @@ char GRID_isFinished(char grid[][6])
 		currentPlayerChecking = grid[i][0];
 		for(j=0 ; j<6 && nbCoins<4 ; j++)
 		{	
-			if(currentPlayerChecking != 0 && grid[i][j]==currentPlayerChecking)
+			if(grid[i][j] != currentPlayerChecking)
 			{
-				nbCoins++;
-			}else{
-				currentPlayerChecking=grid[i][j];
 				nbCoins=0;
 			}
+			if(grid[i][j] != 0)
+			{
+				nbCoins++;
+			}
+			currentPlayerChecking=grid[i][j];
 		}
 	}
 
@@ -171,16 +179,18 @@ char GRID_isFinished(char grid[][6])
 	for(i=0 ; i<6 && nbCoins<4 ; i++)
 	{
 		nbCoins=0;
-		currentPlayerChecking = grid[i][0];
+		currentPlayerChecking = grid[j][0];
 		for(j=0 ; j<7 && nbCoins<4 ; j++)
 		{	
-			if(currentPlayerChecking != 0 && grid[j][i]==currentPlayerChecking)
+			if(grid[j][i] != currentPlayerChecking)
 			{
-				nbCoins++;
-			}else{
-				currentPlayerChecking=grid[j][i];
 				nbCoins=0;
 			}
+			if(grid[j][i] != 0)
+			{
+				nbCoins++;
+			}
+			currentPlayerChecking=grid[j][i];
 		}
 	}
 
@@ -195,13 +205,15 @@ char GRID_isFinished(char grid[][6])
 		currentPlayerChecking = grid[col][raw];
 		while(col<7 && raw <6 && nbCoins<4)
 		{
-			if(currentPlayerChecking != 0 && grid[col][raw]==currentPlayerChecking)
+			if(grid[col][raw] != currentPlayerChecking)
 			{
-				nbCoins++;
-			}else{
-				currentPlayerChecking=grid[col][raw];
 				nbCoins=0;
 			}
+			if(grid[col][raw] != 0)
+			{
+				nbCoins++;
+			}
+			currentPlayerChecking=grid[col][raw];
 
 			col++;
 			raw++;
@@ -226,13 +238,15 @@ char GRID_isFinished(char grid[][6])
 		currentPlayerChecking = grid[col][raw];
 		while(col<7 && raw >=0 && nbCoins<4)
 		{
-			if(currentPlayerChecking != 0 && grid[col][raw]==currentPlayerChecking)
+			if(grid[col][raw] != currentPlayerChecking)
 			{
-				nbCoins++;
-			}else{
-				currentPlayerChecking=grid[col][raw];
 				nbCoins=0;
 			}
+			if(grid[col][raw] != 0)
+			{
+				nbCoins++;
+			}
+			currentPlayerChecking=grid[col][raw];
 
 			col++;
 			raw--;
